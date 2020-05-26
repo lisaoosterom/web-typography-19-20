042a52dc7c027bf5f6a3b81ab75d530d01fb97a2
lisaoosterom <40243965+lisaoosterom@users.noreply.github.com>
2020-05-26 20:26:00 +0200

    test

--- sounds.js
+++ sounds.js
@@ -1,18 +1,19 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	77,
-104,
-104,
-130
+	0, // 0 dreigende muziek
+	9, // 1 buzzer
+	12, // 2 baseline / sirene
+	15, // 3 lazer
+	16.5, // 4 
+	24, // 5 skew > skin job
+	25, // 6  
+	31, // 7 cirkle 
+	32, // 8 background color > green
+	33, // 9 piep
+	77, // 10 grey scale 
+85, // 11
+104, // 12 bliep bliep 
+125,// 13 scale eind
+130 //14
 ];
